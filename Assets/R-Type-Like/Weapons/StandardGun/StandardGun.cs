1ff8807dfb8500fea7bc3fa0ad02e01c69aec8f6
tranquillo <rob.tranquillo@gmx.de>
2022-10-16 19:07:42 +0200

    add StandardGun

---
+++ Assets/R-Type-Like/Weapons/StandardGun/StandardGun.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StandardGun : MonoBehaviour
+{
+    public GameObject spark;
+    private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+    public ParticleSystem particleSysGun;
+    public ParticleSystem particleSysMuzzleFlare;
+
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int eventCount = particleSysGun.GetCollisionEvents(other, collisionEvents);
+        for (int i = 0; i < eventCount; i++)
+        {
+            Instantiate(spark, collisionEvents[i].intersection, Quaternion.LookRotation(collisionEvents[i].normal));
+        }
+    }
+
+    private void FireStop(InputAction.CallbackContext obj)
+    {
+        particleSysMuzzleFlare.Stop();
+        particleSysGun.Stop();
+    }
+
+    private void FireStart(InputAction.CallbackContext obj)
+    {
+        particleSysMuzzleFlare.Play();
+        particleSysGun.Play();
+    }
+
+    #region - Input System -
+    private InputKeyboard inputKeyboard;
+    private void OnEnable()
+    {
+        inputKeyboard.ShipControl.Enable();
+    }
+    private void OnDisable()
+    {
+        inputKeyboard.ShipControl.Disable();
+    }
+
+    private void Awake()
+    {
+        inputKeyboard = new InputKeyboard();
+        inputKeyboard.ShipControl.Fire.started  += FireStart;
+        inputKeyboard.ShipControl.Fire.canceled += FireStop;
+    }
+    #endregion - Input System -
+}
