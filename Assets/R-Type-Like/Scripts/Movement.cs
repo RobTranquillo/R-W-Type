b315aa8859bd25f19c2a3d99575b88bc29aab78c
tranquillo <rob.tranquillo@gmx.de>
2022-10-20 19:27:45 +0200

    add obstacle rotation and do fixed update for movement

--- Assets/R-Type-Like/Scripts/Movement.cs
+++ Assets/R-Type-Like/Scripts/Movement.cs
@@ -1,15 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float movingSpeed;
 
+    public float rotatingTime = 5f;
+    [Range(0, 180)]
+    public float rotationX_Axis;
+    [Range(0, 180)]
+    public float rotationY_Axis;
+    [Range(0, 180)]
+    public float rotationZ_Axis;
 
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 direction = new Vector3(-1,0,0);
-        transform.position = transform.position + direction * speed;
+        transform.position = transform.position + Vector3.left * movingSpeed;
+    }
+
+    private void Start()
+    {
+        if ((rotationX_Axis + rotationY_Axis + rotationZ_Axis + rotatingTime) == 0)
+            return;
+        StartCoroutine(Rotate());
+    }
+
+    private IEnumerator Rotate()
+    {
+        float timeElapsed = 0;
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(rotationX_Axis, rotationY_Axis, rotationZ_Axis);
+        while (timeElapsed < rotatingTime)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / rotatingTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = targetRotation;
+        StartCoroutine(Rotate());
     }
 }
