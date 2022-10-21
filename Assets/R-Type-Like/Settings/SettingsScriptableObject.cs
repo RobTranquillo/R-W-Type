41a6731efedacd07ae410b088ce8f3bd00e6d59c
tranquillo <rob.tranquillo@gmx.de>
2022-10-21 19:03:20 +0200

    add Settings in Scriptable object and switchable

---
+++ Assets/R-Type-Like/Settings/SettingsScriptableObject.cs
@@ -0,0 +1,84 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+/// <summary>
+/// Singleton class to get acces to the active game settings
+/// 
+/// ! Can not use in Awake() ! Earliest access in Start() possible.
+/// 
+/// Structure:
+/// ---------
+/// Settings should be named as same as the class 
+/// for what they are and also there properties
+/// 
+/// 
+/// Usage:
+/// -----
+/// 
+/// private void Start()
+/// {
+///     horizontalSpeed = SettingsController.Active().PlayerController.HorizontalSpeed;
+///     verticalSpeed = SettingsController.Active().PlayerController.VerticalSpeed;
+/// }
+/// 
+/// </summary>
+[CreateAssetMenu(menuName = "R-W-Type/SettingsScriptableObject")]
+public class SettingsScriptableObject : ScriptableObject
+{
+    [Serializable]
+    public class PlayerControllerSettings
+    {
+        public float VerticalSpeed;
+        public float HorizontalSpeed;
+    }
+    public SettingsScriptableObject.PlayerControllerSettings PlayerController;
+
+
+    [Serializable]
+    public class PlayerHealthSettings
+    {
+        public float ShoipIntegrity;
+    }
+    public SettingsScriptableObject.PlayerHealthSettings PlayerHealth;
+
+
+    [Serializable]
+    public class EnemySpawnerSettings
+    {
+        public float spawnBaseIntervall;
+        public bool increaseEnemyCount;
+    }
+    public SettingsScriptableObject.EnemySpawnerSettings EnemySpawner;
+    
+    [Serializable]
+    public class ObstacleSpawnerSettings
+    {
+        public float BaseSpawnIntervall;
+        public float MaxCount;
+    }
+    public SettingsScriptableObject.ObstacleSpawnerSettings ObstacleSpawner;
+    
+    [Serializable]
+    public class PowerUpSpawnerSettings
+    {
+        public float BaseSpawnIntervall;
+    }
+    public SettingsScriptableObject.PowerUpSpawnerSettings PowerUpSpawner;
+    
+    [Serializable]
+    public class StandardGunSettings
+    {
+        public float DamageOnEnemies;
+    }
+    public SettingsScriptableObject.StandardGunSettings StandardGun;
+    
+    [Serializable]
+    public class HitDetectorSettings
+    {
+        public float EnemiesDamageOnCollision;
+    }
+    public SettingsScriptableObject.HitDetectorSettings HitDetector;
+
+}
