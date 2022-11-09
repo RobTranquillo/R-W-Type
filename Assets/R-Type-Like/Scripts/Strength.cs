6efa13b3ae096d95b6bde5663786fc3ecaf929f2
tranquillo <rob.tranquillo@gmx.de>
2022-11-09 14:45:13 +0100

    make enemys shake and explode

--- Assets/R-Type-Like/Scripts/Strength.cs
+++ Assets/R-Type-Like/Scripts/Strength.cs
@@ -1,7 +1,7 @@
+using DG.Tweening;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Strength : MonoBehaviour
 {
@@ -12,12 +12,22 @@ public class Strength : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip soundIfDestroyed;
     public AudioClip soundOnHit;
+    public GameObject destroyParticleSystem;
+
+    [Header("Interims FX")]
     [Tooltip("Do not play hit sound on every hit, but on every X hit")]
     public int soundOnlyEveryXHit = 1;
+    [Tooltip("Shake on every X hit")]
+    public bool shakeRandomOnXHit = true;
+    public bool rotateRandomOnXHit = true;
+    public float shakeDuration = 0.6f;
+    public float shakeStrength = 0.6f;
+    public int shakeVibratio = 10;
+    public float shakeRandomness = 0.6f;
+    public float destroyDelay = 0.3f;
 
     internal Action onDestroy;
     internal int lastHits = 1;
-
 
     private void Start()
     {
@@ -31,16 +41,33 @@ public class Strength : MonoBehaviour
     internal void Damage(float damageStrengthOnEnemies)
     {
         SoundOnHit();
+        ShakeOnHit();
         strength -= damageStrengthOnEnemies;
         if (strength > 0)
             return;
         if (!selfDestroyOnZero)
             return;
 
+        destroyParticleSystem.SetActive(true);
         audioSource.clip = soundIfDestroyed;
         audioSource.Play();
         onDestroy?.Invoke();
+        Invoke("DestroyNow", destroyDelay);
+    }
+
+    private void DestroyNow()
+    {
         Destroy(gameObject);
+    }
+
+    private void ShakeOnHit()
+    {
+        if (!shakeRandomOnXHit)
+            return;
+        transform.DOShakePosition(shakeDuration, shakeStrength, shakeVibratio, shakeRandomness);
+        if (!rotateRandomOnXHit)
+            return;
+        transform.DOShakeRotation(shakeDuration, shakeStrength, shakeVibratio, shakeRandomness);
     }
 
     private void SoundOnHit()
