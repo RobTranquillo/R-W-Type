96f142f62ca9ae5d41692cfd163df562ee94823b
tranquillo <rob.tranquillo@gmx.de>
2022-11-11 11:04:28 +0100

    try fix build version

--- Assets/R-Type-Like/Scripts/BuildVersion.cs
+++ Assets/R-Type-Like/Scripts/BuildVersion.cs
@@ -6,6 +6,6 @@ public class BuildVersion : MonoBehaviour
     private void Awake()
     {
         if (TryGetComponent(out TMPro.TMP_Text output))
-            output.text = "Build: " + Application.version;
+            output.text = $"Build: { Application.version }";
     }
 }
